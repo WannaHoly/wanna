55745dcc4c39768ce25132d8cb800b7a6ca9db49
WannaHoly <2304302348@qq.com>
2023-05-09 20:26:06 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,18 @@
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
+  chainName: 区块链
+  hash：父区块hash值
+  index：区块序号
+  hash：本区块hash值
 
   */
-  constructor() {}
+  constructor(blockChain,preHash,index,hash) {
+    this.blockChain = blockChain
+    this.preHash = preHash
+    this.index = index
+    this.hash = hash
+  }
 }
 
 export default Block
