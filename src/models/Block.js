7ecdd8aeb250b4a01fb57f06d13e5f7aecb44f06
WannaHoly <2304302348@qq.com>
2023-05-16 21:08:03 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -5,11 +5,31 @@ export const DIFFICULTY = 2
 class Block {
   // 1. 完成构造函数及其参数
 
-  constructor() {}
+  constructor(blockChain,preHash,index,hash) {
+    this.blockChain = blockChain
+    this.preHash = preHash
+    this.index = index
+    this.hash = hash
+  }
 
-  isValid() {}
+  isValid() {
+    //获取hash值的前difficulty个位，用于验证
+    let validhash = this.hash.slice(0,DIFFICULTY)
+    //获得验证通过的条件
+    let validStr = ""
+    for (let i=0;i < DIFFICULTY;i++){
+      validStr += "0"
+    }
+    //hash值是否能通过，若至少前diff个位为零就验证通过
+    if (validStr == validhash){
+      return true
+    }
+    return false
+  }
 
-  setNonce(nonce) {}
+  setNonce(nonce) {
+    this.hash = nonce
+  }
   
 }
 
