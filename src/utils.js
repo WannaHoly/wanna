aaf2e9882b2b3f6dc5025cc9f10ead902d79abfd
WannaHoly <2304302348@qq.com>
2023-05-24 11:16:35 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,10 @@
 import sha256 from 'crypto-js/sha256.js'
 
-export const validateHash = () => {}
+export const validateHash = (hash) => {
+  if (hash.length == 0 || !hash) return false
+
+  return hash.length == 64
+}
 
 export const calcNonce = (block) => {
   console.log(`calc nonce of block ${block.height} `)
@@ -10,6 +14,8 @@ export const calcNonce = (block) => {
     block.setNonce(sha256(new Date().getTime().toString()).toString())
     calcTimes++
   }
+  //设置时间间隔，时间间隔过短会导致，nonce的值是相同的，因为实验是用时间戳作为随机数
+  for(let i=0;i < 100000000;i++){}
   const end = new Date().getTime()
   console.log(
     `calc nonce cost ${(end - start) / 1000}s, try ${calcTimes} times`,
