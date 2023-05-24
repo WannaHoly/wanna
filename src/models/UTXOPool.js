8b546ba0b523e1b4647c40e95021676ab78efe14
WannaHoly <2304302348@qq.com>
2023-05-24 11:16:31 +0800

    Update UTXOPool.js

--- src/models/UTXOPool.js
+++ src/models/UTXOPool.js
@@ -1,16 +1,36 @@
 import UTXO from './UTXO.js'
 
 class UTXOPool {
-  constructor(utxos = {}) {}
+  constructor(utxos = {}) {
+    this.utxos = utxos
+  }
 
   // 添加交易函数
   /**
    * 将交易的信息更新至 UTXOPool 中
    */
-  addUTXO() {}
+  addUTXO(publicKey, amount) {
+    if(this.utxos.hasOwnProperty(publicKey)){
+      this.utxos[publicKey].amount += amount 
+    }else{
+      this.utxos[publicKey] = new UTXO(amount)
+    }
+  }
 
   // 将当前 UXTO 的副本克隆
-  clone() {}
+  clone() {
+    return this.utxos
+  }
+
+    // 处理交易函数
+    handleTransaction() {}
+
+    // 验证交易合法性
+    /**
+     * 验证余额
+     * 返回 bool
+     */
+    isValidTransaction() {}
 }
 
 export default UTXOPool
